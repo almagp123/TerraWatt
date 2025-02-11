d4eb2ed2a6d3b6f59f4d2b0f4c61c15dd5d550b0
almagp123 <almagutierrezpc@gmail.com>
2025-02-11 18:10:23 +0100

    commit

--- Terrawatt/Web_TerraWatt/Funciones/accesibilidad.js
+++ Terrawatt/Web_TerraWatt/Funciones/accesibilidad.js
@@ -16,6 +16,37 @@
 //     };
 // });
 
+// // document.addEventListener("DOMContentLoaded", function () {
+// //     const toggleAccesible = document.getElementById("toggleAccesible");
+// //     toggleAccesible.addEventListener("click", function () {
+// //         document.body.classList.toggle("modo-accesible");
+// //     });
+
+// //     window.leerTexto = function (id) {
+// //         // let texto = document.getElementById(id).textContent;
+// //         // let speech = new SpeechSynthesisUtterance();
+// //         let texto = document.getElementById(id).textContent;
+// //         let speech = new SpeechSynthesisUtterance();
+
+// //         speech.text = texto;
+
+// //         // Detecta el idioma del documento y ajusta la voz
+// //         let idioma = document.documentElement.lang;
+// //         if (idioma === "en") {
+// //             speech.lang = "en-US"; // Inglés americano
+// //         } 
+// //         if (idioma === "es") {
+// //             speech.lang = "es-ES"; // Inglés americano
+// //         } 
+// //         else {
+// //             speech.lang = "es-ES"; // Español de España
+// //         }
+
+// //         speech.rate = 1;
+// //         speech.volume = 1;
+// //         window.speechSynthesis.speak(speech);
+// //     };
+// // });
 document.addEventListener("DOMContentLoaded", function () {
     const toggleAccesible = document.getElementById("toggleAccesible");
     toggleAccesible.addEventListener("click", function () {
@@ -31,6 +62,8 @@ document.addEventListener("DOMContentLoaded", function () {
         let idioma = document.documentElement.lang;
         if (idioma === "en") {
             speech.lang = "en-US"; // Inglés americano
+        } else if (idioma === "ar") {
+            speech.lang = "ar-SA"; // Árabe (Arabia Saudita)
         } else {
             speech.lang = "es-ES"; // Español de España
         }
